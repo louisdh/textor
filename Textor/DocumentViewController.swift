2019ed96a94f590a901bb3a614219959e865c968
Louis D'hauwe <louisdhauwe@silverfox.be>
2018-01-20 21:09:24 +0100

    Update icon

---
+++ Textor/DocumentViewController.swift
@@ -0,0 +1,51 @@
+//
+//  DocumentViewController.swift
+//  Textor
+//
+//  Created by Louis D'hauwe on 31/12/2017.
+//  Copyright © 2017 Silver Fox. All rights reserved.
+//
+
+import UIKit
+
+class DocumentViewController: UIViewController {
+	
+	@IBOutlet weak var textView: UITextView!
+	var document: Document?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+		
+		self.navigationController?.view.tintColor = .appTintColor
+		view.tintColor = .appTintColor
+
+		textView.text = ""
+        // Access the document
+        document?.open(completionHandler: { (success) in
+            if success {
+				
+				self.textView.text = self.document?.text
+
+                // Display the content of the document, e.g.:
+//                self.documentNameLabel.text = self.document?.fileURL.lastPathComponent
+            } else {
+                // Make sure to handle the failed import appropriately, e.g., by presenting an error message to the user.
+            }
+        })
+    }
+    
+    @IBAction func dismissDocumentViewController() {
+		
+		let currentText = self.document?.text ?? ""
+		
+		self.document?.text = self.textView.text
+		
+		if currentText != self.textView.text {
+			self.document?.updateChangeCount(.done)
+		}
+		
+        dismiss(animated: true) {
+            self.document?.close(completionHandler: nil)
+        }
+    }
+}
