aacf0cd76ae092c5edf7307f2b404b3de35f8301
Filippo Claudi <filippo.claudi@gmail.com>
2018-03-12 17:54:10 +0100

    Update AppDelegate.swift
    fix a bug where on a new install the font was set to 0

--- Textor/AppDelegate.swift
+++ Textor/AppDelegate.swift
@@ -22,6 +22,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		
 		window?.tintColor = .appTintColor
 		
+		let usrDefaults = UserDefaults.standard
+        	if usrDefaults.double(forKey: "fontSize") == 0 {
+			usrDefaults.set(18, forKey: "fontSize")
+        	}
+		
+		
 		#if SCREENSHOTS
 		SDStatusBarManager.sharedInstance().enableOverrides()
 		#endif
