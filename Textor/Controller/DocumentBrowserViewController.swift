e05190b3f2b9f16ae768fa40993c3922b8e8001a
Louis D'hauwe <louisdhauwe@silverfox.be>
2018-03-14 20:15:55 +0100

    Cleanup document browser

--- Textor/Controller/DocumentBrowserViewController.swift
+++ Textor/Controller/DocumentBrowserViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DocumentBrowserViewController: UIDocumentBrowserViewController, UIDocumentBrowserViewControllerDelegate {
+class DocumentBrowserViewController: UIDocumentBrowserViewController {
 
 	override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +22,15 @@ class DocumentBrowserViewController: UIDocumentBrowserViewController, UIDocument
         allowsPickingMultipleItems = false
 
         NotificationCenter.default.addObserver(self, selector: #selector(self.setTheme), name: .themeChanged, object: nil)
+		
         // Update the style of the UIDocumentBrowserViewController
         setTheme()
 
 		view.tintColor = .appTintColor
 
-		self.additionalLeadingNavigationBarButtonItems = [UIBarButtonItem(image: #imageLiteral(resourceName: "Settings"), style: .done, target: self, action: #selector(showSettings))]
+		let settingsBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "Settings"), style: .done, target: self, action: #selector(showSettings))
+		
+		self.additionalLeadingNavigationBarButtonItems = [settingsBarButtonItem]
 	}
 
     @objc func setTheme() {
@@ -84,59 +87,6 @@ class DocumentBrowserViewController: UIDocumentBrowserViewController, UIDocument
 		self.present(navCon, animated: true, completion: nil)
 	}
 
-    // MARK: UIDocumentBrowserViewControllerDelegate
-
-    func documentBrowser(_ controller: UIDocumentBrowserViewController, didRequestDocumentCreationWithHandler importHandler: @escaping (URL?, UIDocumentBrowserViewController.ImportMode) -> Void) {
-        let newDocumentURL: URL? = nil
-
-//		let url = URL(fileURLWithPath: "Untitled.txt")
-
-		let newName = DocumentManager.shared.availableFileName(forProposedName: "Untitled")
-
-		guard let url = DocumentManager.shared.cacheUrl(for: newName) else {
-			importHandler(nil, .none)
-			return
-		}
-
-		let doc = Document(fileURL: url)
-		doc.text = ""
-
-		doc.save(to: url, for: .forCreating) { (_) in
-
-			doc.close(completionHandler: { (_) in
-
-				importHandler(url, .move)
-
-			})
-
-		}
-
-        // Set the URL for the new document here. Optionally, you can present a template chooser before calling the importHandler.
-        // Make sure the importHandler is always called, even if the user cancels the creation request.
-//        if newDocumentURL != nil {
-//            importHandler(newDocumentURL, .move)
-//        } else {
-//            importHandler(nil, .none)
-//        }
-    }
-
-    func documentBrowser(_ controller: UIDocumentBrowserViewController, didPickDocumentURLs documentURLs: [URL]) {
-        guard let sourceURL = documentURLs.first else { return }
-
-        // Present the Document View Controller for the first document that was picked.
-        // If you support picking multiple items, make sure you handle them all.
-        presentDocument(at: sourceURL)
-    }
-
-    func documentBrowser(_ controller: UIDocumentBrowserViewController, didImportDocumentAt sourceURL: URL, toDestinationURL destinationURL: URL) {
-        // Present the Document View Controller for the new newly created document
-        presentDocument(at: destinationURL)
-    }
-
-    func documentBrowser(_ controller: UIDocumentBrowserViewController, failedToImportDocumentAt documentURL: URL, error: Error?) {
-        // Make sure to handle the failed import appropriately, e.g., by presenting an error message to the user.
-    }
-
     // MARK: Document Presentation
 
 	var transitionController: UIDocumentBrowserTransitionController?
@@ -158,6 +108,51 @@ class DocumentBrowserViewController: UIDocumentBrowserViewController, UIDocument
         present(navCon, animated: true, completion: nil)
     }
 
+}
+
+extension DocumentBrowserViewController: UIDocumentBrowserViewControllerDelegate {
+
+	func documentBrowser(_ controller: UIDocumentBrowserViewController, didRequestDocumentCreationWithHandler importHandler: @escaping (URL?, UIDocumentBrowserViewController.ImportMode) -> Void) {
+		
+		let newName = DocumentManager.shared.availableFileName(forProposedName: "Untitled")
+		
+		guard let url = DocumentManager.shared.cacheUrl(for: newName) else {
+			importHandler(nil, .none)
+			return
+		}
+		
+		let doc = Document(fileURL: url)
+		doc.text = ""
+		
+		doc.save(to: url, for: .forCreating) { (_) in
+			
+			doc.close(completionHandler: { (_) in
+				
+				importHandler(url, .move)
+				
+			})
+			
+		}
+		
+	}
+	
+	func documentBrowser(_ controller: UIDocumentBrowserViewController, didPickDocumentURLs documentURLs: [URL]) {
+		guard let sourceURL = documentURLs.first else { return }
+		
+		// Present the Document View Controller for the first document that was picked.
+		// If you support picking multiple items, make sure you handle them all.
+		presentDocument(at: sourceURL)
+	}
+	
+	func documentBrowser(_ controller: UIDocumentBrowserViewController, didImportDocumentAt sourceURL: URL, toDestinationURL destinationURL: URL) {
+		// Present the Document View Controller for the new newly created document
+		presentDocument(at: destinationURL)
+	}
+	
+	func documentBrowser(_ controller: UIDocumentBrowserViewController, failedToImportDocumentAt documentURL: URL, error: Error?) {
+		// Make sure to handle the failed import appropriately, e.g., by presenting an error message to the user.
+	}
+	
 }
 
 extension DocumentBrowserViewController: UIViewControllerTransitioningDelegate {
